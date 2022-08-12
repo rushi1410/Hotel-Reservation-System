8ad95bdcc3c5a13464994dd4c4fce6aa56b3306f
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-12 16:32:45 +0530

    Refactored:To Find The Best Rated Hotel

--- src/main/java/com/bridgelabz/HotelReservationIF.java
+++ src/main/java/com/bridgelabz/HotelReservationIF.java
@@ -11,5 +11,5 @@ public interface HotelReservationIF {
 	public ArrayList<Hotel> getHotelList();
 	public ArrayList<Hotel> getCheapestHotel(LocalDate startDate, LocalDate endDate);
 	public Hotel getCheapestBestRatedHotel(LocalDate startDate, LocalDate endDate);
-
+	public Hotel getBestRatedHotel(LocalDate startDate, LocalDate endDate);
 }
